3df503b4c136e5bb6b8e3781b928c1d8d7a35bb7
rizky267 <jeklondon234@gmail.com>
2024-11-07 13:31:41 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("OrderButton").onclick = function () {
+let foodPrice = parseint(document.getElementById
+    ("food").value);
+let drinkPrice = parseint(document.getElementById
+    ("drink").value);
+
+    let totalprice = foodPrice + drinkPrice;
+
+    let discount = 0;
+
+    if (totalPrice > 90) {
+        alert('ok')
+    }
+    
+}
